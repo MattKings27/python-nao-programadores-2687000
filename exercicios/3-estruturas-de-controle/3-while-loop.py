86a491308efb70e9e061f3c3acac89b65e15fa12
MattKings27 <math.kings.m@gmail.com>
2024-03-23 17:46:12 -0400

    Finish LL

--- exercicios/3-estruturas-de-controle/3-while-loop.py
+++ exercicios/3-estruturas-de-controle/3-while-loop.py
@@ -1,7 +1,13 @@
 # Crie duas variáveis do tipo numérica, uma sinalizando a fase atual do curso e outra o nível final
-
-
+nv_atual = 1
+nv_final = 4
+ 
 # Crie um while loop que imprima na tela o nível atual
 
 
 # Insira "else" no while loop anterior.
+while nv_atual <= nv_final:
+  print(f' Você avançou um nível do curso, seu nível atual é {nv_atual}')
+  nv_atual += 1
+else:
+  print(f'Parabéns, você atingiu o nível {nv_final}, que é o máximo.')
--- exercicios/3-estruturas-de-controle/projeto-fizz-buzz.py
+++ exercicios/3-estruturas-de-controle/projeto-fizz-buzz.py
@@ -1,7 +1,23 @@
 # Criaremos um programa para substituir números por palavras em uma lista
 # 1. Crie uma lista com 15 números
+lista = list(range(1,31))
 # 2. Crie um for loop para percorrer todos os elementos da lista
+print(lista)
+indice = 0
 # 3. Crie uma estrutura condicional para verificar cada número da lista:
+for numbers in lista:
+  if numbers % 5 == 0 and numbers % 3 == 0:
+    lista[indice] = 'FizzBuzz'
+  elif numbers % 3 == 0:
+    lista[indice] = 'Fizz'
+  elif numbers % 5 == 0:
+    lista[indice] = 'Buzz'
+  else: 
+    lista[indice] = numbers
+  indice += 1
+
+print(lista)
+
 # 3.1 Caso o número seja divisível por 3, substitua-o por "Fizz"
 # 3.2 Caso o número seja divisível por 5, substitua-o por "Buzz"
 # 3.3 Caso o número seja divisível por 3 e 5, substitua-o por "FizzBuzz"
